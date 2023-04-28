d7b8b3625ad779d4adf40c20b11d7448dbcb7366
Arantxa Edwards <37158609+arantxa-e@users.noreply.github.com>
2023-04-27 20:58:06 -0400

    Clean up db connection

--- src/db/mongoose.ts
+++ src/db/mongoose.ts
@@ -1,6 +1,5 @@
 import mongoose from "mongoose";
 
 const connectionURL = process.env.MONGODB_URI;
-const dbName = "studyspot";
 
-mongoose.connect(`${connectionURL}/${dbName}`);
+mongoose.connect(connectionURL!);
