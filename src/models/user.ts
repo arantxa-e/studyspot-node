1d2f6bef6fa1f0ed3e0ee4815eac4e61d25855e5
Arantxa Edwards <37158609+arantxa-e@users.noreply.github.com>
2023-04-27 21:55:14 -0400

    Create interfaces for the schema and instance methods Create generateAuthToken instance method

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,6 +1,22 @@
 import mongoose from "mongoose";
+import jwt from "jsonwebtoken";
 
-const userSchema = new mongoose.Schema({
+interface IUser {
+  firstName?: string;
+  lastName?: string;
+  displayName: string;
+  email: string;
+  avatar?: string;
+  location?: string;
+  password?: string;
+  tokens?: Array<{ token: string }>;
+}
+
+interface IUserMethods {
+  generateAuthToken(): Promise<string>;
+}
+
+const userSchema = new mongoose.Schema<IUser, {}, IUserMethods>({
   firstName: String,
   lastName: String,
   displayName: {
@@ -15,7 +31,23 @@ const userSchema = new mongoose.Schema({
   avatar: Buffer,
   location: String,
   password: String,
+  tokens: [
+    {
+      token: {
+        type: String,
+        required: true,
+      },
+    },
+  ],
 });
+
+userSchema.methods.generateAuthToken = async function () {
+  const user = this;
+  const token = jwt.sign({ _id: user._id.toString() }, process.env.JWT_SECRET!);
+  user.tokens = user.tokens.concat({ token });
+  await user.save();
+  return token;
+};
 
 const User = mongoose.model("User", userSchema);
 
