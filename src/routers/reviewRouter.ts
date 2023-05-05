2970f41a942b71a7f05ca9030b35e603e4cfc3e4
Arantxa Edwards <37158609+arantxa-e@users.noreply.github.com>
2023-05-04 21:25:57 -0400

    Add a Create Review route

--- src/routers/reviewRouter.ts
+++ src/routers/reviewRouter.ts
@@ -1,9 +1,21 @@
 import express from "express";
+import auth from "../middleware/auth";
+import Review from "../models/review";
 
 const router = express.Router();
 
-router.get("/review", (req, res) => {
-  res.send("Hello world");
+router.post("/reviews", auth, async (req, res) => {
+  const review = new Review({
+    ...req.body,
+    user: req.user?._id,
+  });
+
+  try {
+    await review.save();
+    res.status(201).send(review);
+  } catch (err) {
+    res.status(400).send(err);
+  }
 });
 
 export default router;
