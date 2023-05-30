7686d1ceda80976823f197760b7f15a15c4640ae
Arantxa Edwards <37158609+arantxa-e@users.noreply.github.com>
2023-05-30 17:36:02 -0400

    Add routes for partner

--- src/routers/partnerRouter.ts
+++ src/routers/partnerRouter.ts
@@ -1,9 +1,71 @@
 import express from "express";
+import Partner, { IPartner } from "../models/partner";
+import auth from "../middleware/auth";
 
 const router = express.Router();
 
-router.get("/partner", (req, res) => {
-  res.send("Hello world");
+router.post("/partner", async (req, res) => {
+  try {
+    const partner = new Partner(req.body);
+    if (!partner) res.status(400).send();
+    const token = await partner.generateAuthToken();
+    res.status(201).send({ partner, token });
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
+
+router.post("/partner/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      return res
+        .status(400)
+        .send(`Please enter a valid ${!email ? "email" : "password"}`);
+    }
+    const partner = await Partner.findByCredentials(email, password);
+    const token = await partner.generateAuthToken();
+    res.send({ partner, token });
+  } catch (err) {
+    res.status(400).send({ error: "Unable to login" });
+  }
+});
+
+router.get("/partner/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const partner = await Partner.findById(id);
+    if (!partner) return res.status(404).send();
+    res.send(partner);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
+
+router.patch("/partner/profile", auth, async (req, res) => {
+  try {
+    const validParams: Array<keyof IPartner> = ["company", "email", "password"];
+    const isValidRequest = Object.keys(req.body).every((param) =>
+      validParams.includes(param as keyof IPartner)
+    );
+    if (!isValidRequest)
+      return res.status(400).send({ error: "Invalid params" });
+
+    const updatedPartner = await Partner.findByIdAndUpdate(
+      req.foundUser?._id,
+      req.body,
+      {
+        new: true,
+        runValidators: true,
+      }
+    );
+    if (!updatedPartner) {
+      res.status(404).send();
+    }
+    res.send(updatedPartner);
+  } catch (err) {
+    res.status(500).send(err);
+  }
 });
 
 export default router;
