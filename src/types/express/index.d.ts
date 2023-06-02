dbb6fc2851bfcf9b759c591700e139cdb4db5ece
Arantxa Edwards <37158609+arantxa-e@users.noreply.github.com>
2023-06-02 10:30:57 -0400

    Fix types to access schema properties

--- src/types/express/index.d.ts
+++ src/types/express/index.d.ts
@@ -1,11 +1,15 @@
 import { Document } from "mongoose";
-import { GeocodedAddress } from "geocodio-library-node";
+import { IUser } from "../../models/user";
+import { IPartner } from "../../models/partner";
+
+interface DocumentUser extends IUser, Document {}
+interface DocumentPartner extends IPartner, Document {}
 
 declare global {
   namespace Express {
     export interface Request {
-      user?: Document;
-      partner?: Document;
+      user?: DocumentUser;
+      partner?: DocumentPartner;
       token?: string;
       coordinates?: [number, number];
     }
