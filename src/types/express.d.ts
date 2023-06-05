ec347c8b21efd4b73f0c8df601342cad7daa94b7
Arantxa Edwards <37158609+arantxa-e@users.noreply.github.com>
2023-06-05 10:41:29 -0400

    Modify paths for declaration files

--- src/types/express.d.ts
+++ src/types/express.d.ts
@@ -1,6 +1,6 @@
 import { Document } from "mongoose";
-import { IUser } from "../../models/user";
-import { IPartner } from "../../models/partner";
+import { IUser } from "../models/user";
+import { IPartner } from "../models/partner";
 
 interface DocumentUser extends IUser, Document {}
 interface DocumentPartner extends IPartner, Document {}
