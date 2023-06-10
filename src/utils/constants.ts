859af08541f9e473ef5cd78ddc6a0d56461890fa
Arantxa Edwards <37158609+arantxa-e@users.noreply.github.com>
2023-06-09 22:02:34 -0400

    Add notFound error message

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -6,4 +6,5 @@ export const errorMessages = {
   unauthorized: "The user is not authenticated.",
   invalidPatch: "Unable to find and update the resource.",
   invalidDelete: "Unable to find and delete the resource.",
+  notFound: "The resource could not be found.",
 };
