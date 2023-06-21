2bfcf51685ed32294c276f15487d75075bec2ec5
Arantxa Edwards <37158609+arantxa-e@users.noreply.github.com>
2023-06-21 17:43:24 -0400

    Add tests for userRouter endpoint
    Add tests for:
    - POST /user
    - POST /user/login
    - POST /user/logout
    - GET /user/profile

--- tests/fixtures/routers/userRouter.test.ts
+++ tests/fixtures/routers/userRouter.test.ts
@@ -2,10 +2,11 @@ import request from "supertest";
 import app from "../../../src/app";
 import User from "../../../src/models/user";
 import { setupDatabase } from "../db";
+import { mockUser, mockUserId } from "../users";
 
 beforeEach(setupDatabase);
 
-describe("post /user", () => {
+describe("POST /user", () => {
   it("should create and authenticate a user", async () => {
     const response = await request(app)
       .post("/user")
@@ -34,7 +35,61 @@ describe("post /user", () => {
     expect(user?.password).not.toBe("MyPass777!");
   });
 
-  it("should throw an error if a user is not provided", async () => {
+  it("should throw an error if a valid user is not provided", async () => {
     await request(app).post("/user").expect(500);
+  });
+});
+
+describe("POST /user/login", () => {
+  it("should login an existing user", async () => {
+    const response = await request(app)
+      .post("/user/login")
+      .send({
+        email: mockUser.email,
+        password: mockUser.password,
+      })
+      .expect(200);
+    const user = await User.findById(mockUserId);
+    expect(response.body.token).toBe(user!.tokens![1].token);
+  });
+
+  it("should not login a non-existing user", async () => {
+    await request(app)
+      .post("/user/login")
+      .send({
+        email: "someemail@email.com",
+        password: "somepass",
+      })
+      .expect(400);
+  });
+
+  it("should throw an error if an email or password is not provided", async () => {
+    await request(app).post("/user/login").expect(400);
+  });
+});
+
+describe("POST /user/logout", () => {
+  it("should logout a logged-in user", async () => {
+    await request(app)
+      .post("/user/logout")
+      .set("Authorization", `Bearer ${mockUser.tokens[0].token}`)
+      .expect(200);
+  });
+
+  it("should throw an error if the user is not logged-in", async () => {
+    await request(app).post("/user/logout").expect(401);
+  });
+});
+
+describe("GET /user/profile", () => {
+  it("should retrieve the logged-in user's profile", async () => {
+    await request(app)
+      .get("/user/profile")
+      .set("Authorization", `Bearer ${mockUser.tokens[0].token}`)
+      .expect(200);
+  });
+
+  it("should throw an error if the user is not logged-in", async () => {
+    await request(app).get("/user/profile").expect(401);
   });
 });
